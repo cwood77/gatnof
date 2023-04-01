7082404da9401e5a6483e3e5a90c99b774714104
cwood77 <dumuzi77@gmail.com>
2023-04-01 15:04:25 +0100

    more play skeleton

--- src/shell/verb.play.cpp
+++ src/shell/verb.play.cpp
@@ -1,6 +1,9 @@
 #define WIN32_LEAN_AND_MEAN
+#include "../cmn/autoPtr.hpp"
 #include "../console/arg.hpp"
 #include "../console/log.hpp"
+#include "../file/api.hpp"
+#include "../file/manager.hpp"
 #include "../tcatlib/api.hpp"
 #include <memory>
 
@@ -35,6 +38,22 @@ protected:
 
 void playCommand::run(console::iLog& l)
 {
+   tcat::typePtr<file::iFileManager> fMan;
+   l.writeLnDebug("loading config settings (optional)");
+   cmn::autoReleasePtr<file::iSstFile> pFile(&fMan->bindFile<file::iSstFile>(
+      file::iFileManager::kExeAdjacent,
+      "config.sst",
+      file::iFileManager::kReadOnly
+   ));
+   pFile->tie(l);
+
+   l.writeLnDebug("loading db");
+
+   l.writeLnDebug("initializing cui");
+
+   l.writeLnDebug("contacting server for account info");
+
+   l.writeLnDebug("switching to cui");
 }
 
 } // anonymous namespace
