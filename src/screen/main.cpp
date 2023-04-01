426a7edb53c2550c96e4595f6a8769090f8fa2bc
cwood77 <dumuzi77@gmail.com>
2023-04-01 23:35:40 +0100

    added serviceManager and using for pen

--- src/screen/main.cpp
+++ src/screen/main.cpp
@@ -1,6 +1,13 @@
 #define WIN32_LEAN_AND_MEAN
+#include "../cmn/service.hpp"
 #include "../tcatlib/api.hpp"
 #include <windows.h>
+
+namespace cmn {
+
+tcatExposeSingletonTypeAs(serviceManager,serviceManager);
+
+} // namespace cmn
 
 tcatImplServer();
 
--- src/shell/verb.play.cpp
+++ src/shell/verb.play.cpp
@@ -1,8 +1,10 @@
 #define WIN32_LEAN_AND_MEAN
 #include "../cmn/autoPtr.hpp"
+#include "../cmn/service.hpp"
 #include "../console/arg.hpp"
 #include "../console/log.hpp"
 #include "../cui/api.hpp"
+#include "../cui/pen.hpp"
 #include "../file/api.hpp"
 #include "../file/manager.hpp"
 #include "../tcatlib/api.hpp"
@@ -47,6 +49,11 @@ void playCommand::run(console::iLog& l)
       file::iFileManager::kReadOnly
    ));
    pFile->tie(l);
+
+   l.writeLnDebug("compiling services");
+   tcat::typePtr<cmn::serviceManager> svcMan;
+   pen::object _pen(std::cout);
+   cmn::autoService<pen::object> _penSvc(*svcMan,_pen);
 
    l.writeLnDebug("loading db");
 
